111affd13c41041c5ecf27c80935a173d64495cc
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-06-30 19:21:51 -0700

    project upload init

---
+++ data/aboutMeData.js
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const aboutMeData = [
+  {
+    id: uuidv4(),
+    bio: `Detail-oriented full-stack software engineer. With experience creating and
+implementing modern, semantically structured web applications using a wide range of JavaScript and Node.js frameworks including React, Express, MongoDB. Proven ability to collaborate with teams, apply agile methodologies, and meet tight deadlines. Seeking a challenging role with a company that values creativity and innovation, to continue pushing my skills further.`,
+  },
+  {
+    id: uuidv4(),
+    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+  },
+]
